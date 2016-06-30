1d22c7581ce1c2b56973e3fd71328cb1c7d38f9d
DevinFlick <DevinJFlick@gmail.com>
2016-06-29 21:01:42 -0400

    grid init fixed

--- public/js/app.js
+++ public/js/app.js
@@ -3,10 +3,16 @@ $(document).ready(function(){
   var container = $('.container');
   var numOfRows = $('#numOfRows');
   var numOfCols = $('#numOfCols');
+
   var plusColBtn = $('#plusCol');
   var minusColBtn = $('#minusCol');
   var plusRowBtn = $('#plusRow');
   var minusRowBtn = $('#minusRow');
+
+  var initGridBtn = $('#initGridBtn');
+  var colorClasses = ['white', 'red', 'green', 'blue'];
+  var colorCycle = colorClasses.length-4;
+
 
   initGrid();
   addClickHandlers();
@@ -16,12 +22,14 @@ $(document).ready(function(){
   plusRowBtn.on('click', addRow);
   minusRowBtn.on('click', minusRow);
 
+
 function addCol(){
   if(+numOfCols.text() > 101){
     numOfCols.text("10");
   } else {
   numOfCols.text(+numOfCols.text() + 1);
   }
+  initGrid();
 }
 function minusCol(){
   if(+numOfCols.text() <= 11){
@@ -29,6 +37,7 @@ function minusCol(){
   } else {
     numOfCols.text(+numOfCols.text() -1);
   }
+  initGrid();
 }
 function addRow(){
   if(+numOfRows.text() > 100){
@@ -36,6 +45,7 @@ function addRow(){
   } else {
     numOfRows.text(+numOfRows.text() + 1);
   }
+  initGrid();
 }
 function minusRow(){
   if(+numOfRows.text() <= 11){
@@ -43,25 +53,35 @@ function minusRow(){
   } else {
     numOfRows.text(+numOfRows.text() - 1);
   }
+  initGrid();
 }
 
 function addClickHandlers(){
   var cells = $('.cell');
   cells.on('click', changeColor);
+}
+
 
 function changeColor( ){
-  var colorClasses = ['white', 'red', 'green', 'blue'];
-  var colorCycle = Math.round(Math.random() * (colorClasses.length-1));
+  if (+colorCycle.text < 3){
+    +colorCycle.text() + 1;
+  } else {
+    colorCycle.text = (colorClasses.length - 4);
+  }
   var color = colorClasses [colorCycle];
   $(this).removeClass(colorClasses.join(' '));
   $(this).addClass(color);
+  console.log(colorCycle);
 }
-  }
+
+
+
   function initGrid(){
-    for(var i = 0; i< +numOfRows; i += 1){
+    container.html('');
+    for(var i = 0; i< +numOfRows.text(); i += 1){
       var row = $('<div></div>');
       row.addClass('row');
-      for(var j = 0; j < +numOfCols; j += 1){
+      for(var j = 0; j < +numOfCols.text(); j += 1){
         var cell = $('<div></div>');
         cell.addClass('cell border');
         row.append(cell);
